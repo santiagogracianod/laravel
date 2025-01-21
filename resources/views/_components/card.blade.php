5d9b82f889aa4831f63620a1539382287f864f54
Santiago Graciano <santiago.gracianod@gmail.com>
2025-01-20 22:42:33 -0500

    some styles for cards

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,4 +1,4 @@
-<div style="border: 1px solid black; border-radius: 0.5%; padding: 1%; margin: 2%;">
+<div class="card">
     <h3> {{ $title }} </h3>
     <img src=" {{ asset('assets/img/tree.jpg') }} " alt="" width="120">
     <p> {{ $content }} </p>
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,22 +4,27 @@
 @section('content')
     <h1>Our Services</h1>
 
-    @component('_components.card')
-        @slot('title')
-            Service 1
-        @endslot
+    <div class="cards">
+        @component('_components.card')
+            @slot('title')
+                Service 1
+            @endslot
 
-        @slot('content')
-            This is the content for service 1
-        @endslot
+            @slot('content')
+                This is the content for service 1
+            @endslot
 
-    @endcomponent
+        @endcomponent
 
-    @component('_components.card')
-        @slot('title', 'Service 2')
+        @component('_components.card')
+            @slot('title', 'Service 2')
 
-        @slot('content', 'This is the content for service 2')
+            @slot('content', 'This is the content for service 2')
 
-    @endcomponent
+        @endcomponent
+
+    </div>
+
+
 
 @endsection
