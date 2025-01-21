feb0839af88e06267ffd7b4506ae4c6aac7d8e0f
Santiago Graciano <santiago.gracianod@gmail.com>
2025-01-20 21:36:00 -0500

    ruotes with name

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 //metedo estatico para pintar una vista sin necesidad de un controlador
 //solo aplica siempre que sea una vista estatica
 Route::view('/', 'index')->name('index');
-Route::view('/about', 'about')->name('about');
-Route::view('/services', 'services')->name('services');
-Route::view('/contact', 'contact')->name('contact');
+Route::view('/about-us', 'about')->name('about');
+Route::view('/out-services', 'services')->name('services');
+Route::view('/contact-us', 'contact')->name('contact');
 
 
 
