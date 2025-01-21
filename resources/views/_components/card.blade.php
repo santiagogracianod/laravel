893a01185c42f95aec3c611be565edd48dab888a
Santiago Graciano <santiago.gracianod@gmail.com>
2025-01-20 22:22:09 -0500

    static resources -> asset

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,4 +1,5 @@
-<div>
+<div style="border: 1px solid black; border-radius: 0.5%; padding: 1%; margin: 2%;">
     <h3> {{ $title }} </h3>
+    <img src=" {{ asset('assets/img/tree.jpg') }} " alt="" width="120">
     <p> {{ $content }} </p>
 </div>
